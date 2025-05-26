a89e7700bc6feb8e0e20b3ca9064fa72ad3a2468
shiroo <86618821+shiroHSG@users.noreply.github.com>
2025-05-26 18:49:36 +0900

    chat entity update

--- backend_spring/src/main/java/com/buddy/pium/entity/chat/ChatRoom.java
+++ backend_spring/src/main/java/com/buddy/pium/entity/chat/ChatRoom.java
@@ -1,19 +1,42 @@
-//package com.buddy.pium.entity.chat;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.EntityListeners;
-//import lombok.Builder;
-//import lombok.Getter;
-//import lombok.Setter;
-//import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-//
-//@Entity
-//@Getter
-//@Setter
-//@Builder
-//@EntityListeners(AuditingEntityListener.class)
-//public class ChatRoom {
-//    private Long id;
-//
-//    private Member sender;
-//}
+package com.buddy.pium.entity.chat;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class ChatRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private boolean isGroup;
+
+    @Column(name = "chatroom_name")
+    private String chatRoomName;
+
+    private String password;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "last_message", columnDefinition = "TEXT")
+    private String lastMessageContent;
+
+    @Column(name = "last_sent_at")
+    private LocalDateTime lastMessageSentAt;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+}
---
+++ backend_spring/src/main/java/com/buddy/pium/entity/chat/ChatRoomMember.java
@@ -0,0 +1,38 @@
+package com.buddy.pium.entity.chat;
+
+import com.buddy.pium.entity.member.Member;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class ChatRoomMember {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id", nullable = false)
+    private ChatRoom chatRoom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(nullable = false)
+    private boolean isAdmin;
+
+    @CreatedDate
+    @Column(name = "joined_at", nullable = false)
+    private LocalDateTime joinedAt;
+}
