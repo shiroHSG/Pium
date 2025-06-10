7bb1ac6e6a2aa7cb54824a69dbf61e8afbbbb020
tdchoi <ctd2580@naver.com>
2025-06-10 11:42:22 +0900

    .

--- backend_spring/src/main/java/com/buddy/pium/controller/common/MemberController.java
+++ backend_spring/src/main/java/com/buddy/pium/controller/common/MemberController.java
@@ -23,7 +23,7 @@ public class MemberController {
     /**
      * 회원 가입
      */
-    @PostMapping("/register")
+    @PostMapping()
     public ResponseEntity<MemberResponseDto> create(@RequestBody MemberRegisterDto registerDto) {
         MemberResponseDto responseDto = memberService.createMember(registerDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
