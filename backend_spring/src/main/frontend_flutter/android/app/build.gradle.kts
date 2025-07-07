80edfb2aacda243de513f667272dac0e989c191c
tdchoi <ctd2580@naver.com>
2025-07-07 12:32:33 +0900

    .

--- backend_spring/src/main/frontend_flutter/android/app/build.gradle.kts
+++ backend_spring/src/main/frontend_flutter/android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.test.flutter-test-92811"
+        // applicationId = "com.test.flutter-test-92811"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
