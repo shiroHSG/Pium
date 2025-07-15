fa44663a18a4a2357949f3874026ac7b94367f4e
shiroo <86618821+shiroHSG@users.noreply.github.com>
2025-07-15 21:18:25 +0900

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -3,7 +3,8 @@
 echo "Stopping application..."
 
 APP_DIR=/home/ubuntu/app/pium/zip/backend_spring/build/libs
-JAR_NAME=$(ls $APP_DIR | grep '.jar' | head -n 1)
+# 실행 가능한 jar만 대상으로
+JAR_NAME=$(ls $APP_DIR | grep '.jar' | grep -v 'plain' | head -n 1)
 PID=$(pgrep -f $JAR_NAME)
 
 if [ -z "$PID" ]; then
